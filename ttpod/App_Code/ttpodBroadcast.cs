d1f923176162eb5d9f3b4207b06052a125189557
bajdcc <bajdcc@foxmail.com>
2015-08-30 22:21:12 +0800

    add wcf service and signalr

---
+++ ttpod/App_Code/ttpodBroadcast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+public class ttpodBroadcast : Hub
+{
+	[HubMethodName("BroadcastToPlay")]
+	public void NotifyAll(string type, string title, string url)
+	{
+		Clients.All.playByNotified(type, title, url);
+	}
+}
